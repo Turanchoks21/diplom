492ee97f9f8864eee65d1dd7adc04a9de127f3b4
Turanchoks21 <manjak2111@gmail.com>
2024-05-24 17:28:17 +0300

    feat: created projeckt

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,11 @@
+function NomeView() {
+  return (
+    <>
+      <span className="text-3xl text-red-600 md:text-green-400 sm:text-blue-700">
+        IT`S WORK!
+      </span>
+    </>
+  );
+}
+
+export default NomeView;
