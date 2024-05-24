e8d07c7507697dcb3519fee5e562ce30ac4da5ba
Turanchoks21 <manjak2111@gmail.com>
2024-05-24 21:23:27 +0300

    feat: added router paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import NavBar from "./componets/navigations/NavBar.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeView from "./views/HomeView.jsx";
+import RegisterView from "./views/RegisterView.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeView />,
+      },
+      {
+        path: "register",
+        element: <RegisterView />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NavBar />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
+
