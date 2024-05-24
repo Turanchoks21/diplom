961e7161b9579fe8c72fe0ae4d55026a7d16c732
Turanchoks21 <manjak2111@gmail.com>
2024-05-24 21:21:33 +0300

    feat: created NavButton

---
+++ src/componets/buttons/NavButton.jsx
@@ -0,0 +1,9 @@
+function NavButton({ children }) {
+  return (
+    <>
+      <button className="text-violet-purple hover:text-hot-pink dark:text-neon-green dark:hover:text-hot-pink ">{children}</button>
+    </>
+  );
+}
+
+export default NavButton;
