7704fe1783a8eecefd299ffd5607fccc00b31a57
Turanchoks21 <manjak2111@gmail.com>
2024-05-24 21:24:14 +0300

    feat: created components "Register"

---
+++ src/views/RegisterView.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RegisterView() {
+  return (
+    <>
+      <div className="mt-[20rem]">Yo! you are on register page!</div>
+    </>
+  );
+}
+
+export default RegisterView;
