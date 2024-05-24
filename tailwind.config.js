6500cd1df6314b9ef3a71a89f2251671974c6aba
Turanchoks21 <manjak2111@gmail.com>
2024-05-24 21:20:13 +0300

    feat: added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "midnight-black": "#1a1c22", //light
+        "violet-purple": "#8b45ef", // light
+        "pale-yellow": "#efec88", // light
+        "mint-blue": "#e2fffd", // dark
+        "hot-pink": "#dc3d9d", //dark
+        "neon-green": "#a8ed00", //dark
+      },
+    },
   },
   plugins: [],
 };
