0a15f526aceb867e5670cfa7b4d6c2fb77a47247
Turanchoks21 <manjak2111@gmail.com>
2024-05-26 02:23:04 +0300

    feat(views): added inputs and buttons

--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import UserForm from "../componets/forms/UserForm";
+import FormInput from "../componets/inputs/FormInput";
+import FormButtonOutline from "../componets/buttons/FormButtonOutline";
+import FormButtonSolid from "../componets/buttons/FormButtonSolid";
 
 function RegisterView() {
+  const { t } = useTranslation();
+
   return (
-    <>
-      <div className="mt-[20rem]">Yo! you are on register page!</div>
-    </>
+    <UserForm>
+      <span className="text-xl text-pale-yellow">{t('registerTitle')}</span>
+      <FormInput>{t('nickname')}</FormInput>
+      <FormInput>Email</FormInput>
+      <FormInput>{t('password')}</FormInput>
+      <FormInput>{t('data')}</FormInput>
+      <FormButtonSolid>{t('registerUser')}</FormButtonSolid>
+      <FormButtonOutline to='/login'>{t('haveAccount')}</FormButtonOutline>
+    </UserForm>
   );
 }
 
