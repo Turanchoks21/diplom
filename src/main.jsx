e364a61a18245c5a394c4978c77a942b285a82cb
Turanchoks21 <manjak2111@gmail.com>
2024-05-26 02:21:26 +0300

    feat: added path to "login" and i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import "./i18n";
+import App from "./App.jsx";
 import HomeView from "./views/HomeView.jsx";
 import RegisterView from "./views/RegisterView.jsx";
+import LoginView from "./views/LoginView.jsx";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,10 @@ const router = createBrowserRouter([
         path: "register",
         element: <RegisterView />,
       },
+      {
+        path: "login",
+        element: <LoginView />,
+      },
     ],
   },
 ]);
@@ -28,4 +34,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
