504611219688d9a4c4f523898b3a59c99626512a
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 21:43:47 +0300

    feat: changed colors and added sreens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,24 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      "sm" : "640px",
+      "md" : "768px",
+      "lg" : "1024px",
+      "xl" : "1280px",
+      "2xl" : "1536px",
+      "xxl" : "1920px",
+    },
     extend: {
       colors: {
-        "midnight-black": "#07021e", //dark
-        "violet-purple": "#8b45ef", // dark
-        "pale-yellow": "#efec88", // dark
+        "midnight-black": "#07021e", 
+        "blue-purple": "#4f4dc3",
+        "pale-yellow": "#efec88",
         "lavender-mist": "#f0edff",
-      },     
+      },
     },
+    // 3840x2160 4k
+    // 2048×1080 2k
   },
   plugins: [],
 };
