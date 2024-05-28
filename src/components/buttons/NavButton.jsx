1841be38f54920ad2ed0dc1df4d590803e080002
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 21:42:29 +0300

    feat(components/buttons): added props "onClick"

--- src/components/buttons/NavButton.jsx
+++ src/components/buttons/NavButton.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
 
-function NavButton({ to, children }) {
+function NavButton({ to, children, onClick }) {
   return (
     <>
-      <Link
-        to={to}
-        className="dark:text-pale-yellow text-center hover:text-[#FF5733] text-hot-pink 
-      dark:hover:text-violet-purple text-xl transition ease-in-out hover:scale-110 "
-      >
-        {children}
-      </Link>
+
+        <Link
+          to={to}
+          onClick={onClick}
+          className="dark:text-pale-yellow font-semibold text-midnight-black hover:text-blue-purple  
+        dark:hover:text-blue-purple text-xl xxl:text-3xl transition ease-in-out hover:scale-110"
+        >
+          {children}
+        </Link>
+    
     </>
   );
 }
