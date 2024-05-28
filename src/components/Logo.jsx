e0307a888ba24b10cf04568efe429922410b0505
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 21:41:53 +0300

    feat(components): changed class

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
         className="bg-clip-text text-transparent bg-gradient-to-r 
           from-[#833ab4] via-[#fd1d1d] to-[#fcb045]"
       >
-        <span className="font-bold text-2xl">LevelUp!</span>
+        <div className="font-bold text-2xl xxl:text-4xl">LevelUp!</div>
       </div>
     </>
   );
