3babbe7c2c109e7fe8a8e43529708f9299de6b5f
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 21:41:25 +0300

    feat(components/inputs): added props "type"

--- src/components/inputs/FormInput.jsx
+++ src/components/inputs/FormInput.jsx
@@ -1,9 +1,9 @@
-function FormInput({ children }) {
+function FormInput({ children, type }) {
   return (
-    <div className="form-group w-full max-w-xl">
+    <div className="form-group w-full">
       <input
         className="py-2 pl-3 w-full text-xl text-pale-yellow bg-midnight-black border-2 border-pale-yellow rounded-xl"
-        type="text"
+        type={type}
         placeholder={children}
       />
       <label className="form-label">{children}</label>
