cd6967c437c28133161c9e1d6887febebdd556fa
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 21:36:03 +0300

    feat(componets/buttons): added classes for adaptations

--- src/components/buttons/LanguageSwitcher.jsx
+++ src/components/buttons/LanguageSwitcher.jsx
@@ -21,49 +21,51 @@ export default function Example() {
   };
 
   return (
-    <Popover className="relative">
-      <>
-        <Popover.Button
-          onMouseOver={() => setIsSolid(true)}
-          onMouseLeave={() => setIsSolid(false)}
-          className="inline-flex items-center text-pale-yellow hover:text-violet-purple  
+    <>
+      <Popover className="relative">
+        <>
+          <Popover.Button
+            onMouseOver={() => setIsSolid(true)}
+            onMouseLeave={() => setIsSolid(false)}
+            className=" text-blue-purple dark:text-pale-yellow  
             focus:outline-none focus:ring-0 focus:ring-offset-0"
-        >
-          {isSolid ? (
-            <SolidGlobe className="h-8" />
-          ) : (
-            <OutlineGlobo className="h-8" />
-          )}
-        </Popover.Button>
+          >
+            {isSolid ? (
+              <SolidGlobe className="h-8 xxl:h-12" />
+            ) : (
+              <OutlineGlobo className="h-8 xxl:h-12" />
+            )}
+          </Popover.Button>
 
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="opacity-0 translate-y-1"
-          enterTo="opacity-100 translate-y-0"
-          leave="transition ease-in duration-150"
-          leaveFrom="opacity-100 translate-y-0"
-          leaveTo="opacity-0 translate-y-1"
-        >
-          <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-1 max-w-xs sm:px-0">
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-              <div className="relative grid gap-6 bg-white px-2 py-4 sm:gap-8 sm:p-8">
-                {solutions.map((item) => (
-                  <button
-                    key={item.value}
-                    onClick={() => changeLanguage(item.value)}
-                    className="-m-3 p-1 block rounded-md hover:bg-gray-50 transition ease-in-out duration-150"
-                  >
-                    <p className="text-base font-medium text-gray-900">
-                      {item.name}
-                    </p>
-                  </button>
-                ))}
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-1 max-w-xs sm:px-0">
+              <div className="rounded-lg shadow-lg ring-1 ring-midnight-black ring-opacity-5 overflow-hidden">
+                <div className="relative grid gap-6 bg-white px-2 py-4 sm:gap-8 sm:p-6">
+                  {solutions.map((item) => (
+                    <button
+                      key={item.value}
+                      onClick={() => changeLanguage(item.value)}
+                      className="-m-3 p-1 px-2 block rounded-md hover:bg-gray-200 transition ease-in-out duration-150"
+                    >
+                      <p className="text-base xxl:text-xl font-medium text-midnight-black">
+                        {item.name}
+                      </p>
+                    </button>
+                  ))}
+                </div>
               </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
-      </>
-    </Popover>
+            </Popover.Panel>
+          </Transition>
+        </>
+      </Popover>
+    </>
   );
 }
--- src/components/buttons/ThemButton.jsx
+++ src/components/buttons/ThemButton.jsx
@@ -1,6 +1,12 @@
-import { useEffect, useState } from 'react';
-import { MoonIcon as SolidMoon, SunIcon as SolidSun } from '@heroicons/react/20/solid';
-import { MoonIcon as OutlineMoon, SunIcon as OutlineSun } from '@heroicons/react/24/outline';
+import { useEffect, useState } from "react";
+import {
+  MoonIcon as SolidMoon,
+  SunIcon as SolidSun,
+} from "@heroicons/react/20/solid";
+import {
+  MoonIcon as OutlineMoon,
+  SunIcon as OutlineSun,
+} from "@heroicons/react/24/outline";
 
 const DarkModeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,18 +17,18 @@ const DarkModeToggle = () => {
     const newDarkMode = !isDarkMode;
     setIsDarkMode(newDarkMode);
     setIsIcons(!isIcons);
-    document.body.classList.toggle('dark', newDarkMode);
+    document.body.classList.toggle("dark", newDarkMode);
   };
 
   useEffect(() => {
     const checkDarkModePreference = () => {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         setIsDarkMode(true);
         setIsIcons(true);
-        document.body.classList.add('dark');
+        document.body.classList.add("dark");
       } else {
         setIsIcons(false);
-        document.body.classList.remove('dark');
+        document.body.classList.remove("dark");
       }
     };
 
@@ -30,26 +36,26 @@ const DarkModeToggle = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <button
-        className="text-pale-yellow hover:text-violet-purple"
+        className="text-blue-purple dark:text-pale-yellow"
         onClick={toggleDarkMode}
         onMouseOver={() => setIsSolid(true)}
         onMouseLeave={() => setIsSolid(false)}
       >
         {isIcons ? (
           isSolid ? (
-            <SolidMoon className="h-8" />
+            <SolidMoon className="h-8 xxl:h-12" />
           ) : (
-            <OutlineMoon className="h-8" />
+            <OutlineMoon className="h-8 xxl:h-12" />
           )
         ) : isSolid ? (
-          <SolidSun className="h-8" />
+          <SolidSun className="h-8 xxl:h-12" />
         ) : (
-          <OutlineSun className="h-8" />
+          <OutlineSun className="h-8 xxl:h-12" />
         )}
       </button>
-    </div>
+    </>
   );
 };
 
