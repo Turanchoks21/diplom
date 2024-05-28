6bc6853fba46b0928a09787bde92d0250ba8720c
Turanchoks21 <manjak2111@gmail.com>
2024-05-28 15:24:28 +0300

    feat(components): added Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+function Logo() {
+  return (
+    <>
+      <div
+        className="bg-clip-text text-transparent bg-gradient-to-r 
+          from-[#833ab4] via-[#fd1d1d] to-[#fcb045]"
+      >
+        <span className="font-bold text-2xl">LevelUp!</span>
+      </div>
+    </>
+  );
+}
+
+export default Logo;
--- src/components/navigations/NavBar.jsx
+++ src/components/navigations/NavBar.jsx
@@ -4,6 +4,8 @@ import ThemButton from "./../buttons/ThemButton";
 import NavButton from "../buttons/NavButton";
 import LanguageSwitcher from "../buttons/LanguageSwitcher";
 import { Bars3Icon } from "@heroicons/react/20/solid";
+import Logo from "../Logo";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   const { t } = useTranslation();
@@ -11,6 +13,41 @@ function NavBar() {
   return (
     <>
       <div
+        className="fixed z-10 w-full mx-auto px-2 sm:px-6 lg:px-48
+      bg-lavender-mist dark:bg-midnight-black  border-b-2
+      border-blue-purple transition-color ease-in-out duration-500
+      text-white
+      "
+      >
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <Bars3Icon className="h-8 text-blue-purple dark:text-pale-yellow" />
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 transition ease-in-out hover:scale-110">
+              <Link to="/">
+                <Logo />
+              </Link>
+            </div>
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex justify-center items-center space-x-4">
+                <NavButton to="/register">{t("register")}</NavButton>
+                <NavButton to="/login">{t("login")}</NavButton>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="flex space-x-3">
+              <LanguageSwitcher />
+              <div className="hidden sm:block">
+                <ThemButton />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div
         className="fixed flex justify-between md:px-48 p-5 py-2 items-center 
         top-0 left-0 border-b-2 w-full bg-lavender-mist dark:bg-midnight-black
         border-blue-purple transition-color ease-in-out duration-500"
@@ -31,7 +68,7 @@ function NavBar() {
           <LanguageSwitcher />
           <ThemButton />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
