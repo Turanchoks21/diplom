019167d9b63052473518bf213de73259445278d5
Turanchoks21 <manjak2111@gmail.com>
2024-06-01 11:40:17 +0300

    feat(components/wrapers): createt wraper for game logo on news post

---
+++ src/components/wrapers/news/GameLogo.jsx
@@ -0,0 +1,11 @@
+function GameLogo({src}) {
+  return (
+    <>
+    <div className="h-12 w-12">
+      <img src={src} className="rounded-full" />
+    </div>
+  </>
+  );
+}
+
+export default GameLogo;
