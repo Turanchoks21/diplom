a38ad26dd47092c0aeb21bf4a61766ec62f200c8
Turanchoks21 <manjak2111@gmail.com>
2024-06-01 11:50:07 +0300

    feat(componnets/wrapers): created wrapers for user nickname

---
+++ src/components/wrapers/usersMenu/UserNick.jsx
@@ -0,0 +1,11 @@
+function UserNick({ children }) {
+  return (
+    <>
+      <div className="font-semibold text-midnight-black dark:text-pale-yellow">
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default UserNick;
