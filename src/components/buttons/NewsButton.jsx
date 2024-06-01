e33df506f7717a06a3d6606a864e842d95123acb
Turanchoks21 <manjak2111@gmail.com>
2024-06-01 11:35:47 +0300

    feat(components/buttons): created buttons for news post

---
+++ src/components/buttons/NewsButton.jsx
@@ -0,0 +1,11 @@
+function NewsButton({ children }) {
+  return (
+    <>
+      <div className="text-midnight-black dark:text-pale-yellow">
+        {children}
+      </div>
+    </>
+  );
+}
+
+export default NewsButton;
