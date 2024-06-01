0bf14114ad6246c166e905c69481691416a7c4a1
Turanchoks21 <manjak2111@gmail.com>
2024-06-01 11:35:13 +0300

    feat(components/buttons): added new classes

--- src/components/buttons/NavButton.jsx
+++ src/components/buttons/NavButton.jsx
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 function NavButton({ to, children, onClick }) {
   return (
     <>
-
-        <Link
-          to={to}
-          onClick={onClick}
-          className="dark:text-pale-yellow font-semibold text-midnight-black hover:text-blue-purple  
+      <Link
+        to={to}
+        onClick={onClick}
+        className="flex items-center space-x-2
+        dark:text-pale-yellow font-semibold text-midnight-black hover:text-blue-purple  
         dark:hover:text-blue-purple text-xl xxl:text-3xl transition ease-in-out hover:scale-110"
-        >
-          {children}
-        </Link>
-    
+      >
+        {children}
+      </Link>
     </>
   );
 }
