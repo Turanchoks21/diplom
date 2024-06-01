c60e7ca0652c004a4918c4003cd2faf6fd62b0c9
Turanchoks21 <manjak2111@gmail.com>
2024-06-01 11:34:01 +0300

    feat(views): added welcome massege

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,8 +1,25 @@
+import ButtonSolid from './../components/buttons/ButtonSolid'
+
 function NomeView() {
   return (
     <>
-
-      <div className="mt-[20rem] text-red-500 dark:text-blue-600">IT`S WORK!</div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative items-center flex flex-col
+          col-span-1 gap-6
+        ">
+          <div
+            className="bg-clip-text text-transparent bg-gradient-to-r 
+          from-[#833ab4] via-[#fd1d1d] to-[#fcb045]
+            font-bold text-5xl xxl:text-9xl py-3
+          "
+          >
+            Start chating!
+          </div>
+            <ButtonSolid to='/login'>
+              Let`s do this
+            </ButtonSolid>
+        </div>
+      </div>
     </>
   );
 }
