9a66775516a26067d8bef4f1cb1a97bac385e4f2
Turanchoks21 <manjak2111@gmail.com>
2024-06-02 21:22:17 +0300

    feat: added classes

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,12 +1,23 @@
 import UserNavBar from "./components/navigations/UserNavBar";
+import UserSideBar from "./components/navigations/UserSideBar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 function MainPageView() {
   return (
     <>
       <UserNavBar />
-      <div className="m-0 py-20 px-2 lg:px-48">
-        <Outlet />
+      <div className="m-0 py-20 px-2 lg:px-48 items-center">
+        <div className="flex xl:space-x-4">
+          <div className="w-full max-w-[18rem] hidden xl:block">
+            <UserSideBar />
+          </div>
+          <div className="w-full">
+            <ScrollToTop>
+              <Outlet />
+            </ScrollToTop>
+          </div>
+        </div>
       </div>
     </>
   );
