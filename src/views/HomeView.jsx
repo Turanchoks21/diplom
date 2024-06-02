4966be0d3a667222b126888a1003f16155da5e94
Turanchoks21 <manjak2111@gmail.com>
2024-06-02 21:29:05 +0300

    feat(views): added localization

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,23 +1,28 @@
-import ButtonSolid from './../components/buttons/ButtonSolid'
+import ButtonSolid from "./../components/buttons/ButtonSolid";
+import { useTranslation } from "react-i18next";
 
 function NomeView() {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-        <div className="relative items-center flex flex-col
+        <div
+          className="relative items-center flex flex-col
           col-span-1 gap-6
-        ">
+        "
+        >
           <div
             className="bg-clip-text text-transparent bg-gradient-to-r 
           from-[#833ab4] via-[#fd1d1d] to-[#fcb045]
             font-bold text-5xl xxl:text-9xl py-3
           "
           >
-            Start chating!
+            {t("welcome")}
           </div>
-            <ButtonSolid to='/login'>
-              Let`s do this
-            </ButtonSolid>
+          <div className="w-full max-w-xl flex">
+            <ButtonSolid to="/login">{t("letsDoThis")}</ButtonSolid>
+          </div>
         </div>
       </div>
     </>
