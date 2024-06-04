1af0c2645471c397ad8ee61d616b2450e1836eb9
Turanchoks21 <manjak2111@gmail.com>
2024-06-04 21:46:46 +0300

    feat(components): createtd items for search friens

---
+++ src/components/FriendsListItem.jsx
@@ -0,0 +1,34 @@
+import GameLogo from "./wrapers/news/GameLogo";
+import GameName from "./wrapers/news/GameName";
+import { UserMinusIcon, ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+
+const FriendListItem = ({ friend }) => {
+  return (
+    <>
+      <div key={friend.id}>
+        <div className="w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-between space-x-3">
+              <GameLogo src={friend.userAvatar} />
+              <div>
+                <GameName text={friend.nickName} />
+                {friend.status === "offline" ? (
+                  <span className="text-red-500">offline</span>
+                ) : (
+                  <span className="text-green-500">online</span>
+                )}
+              </div>
+            </div>
+            <div className="flex space-x-4">
+              <UserMinusIcon className="h-8 xxl:h-12 text-red-500" />
+              <ChatBubbleLeftIcon className="h-8 xxl:h-12 text-midnight-black dark:text-pale-yellow" />
+            </div>
+          </div>
+        </div>
+        <div className="border my-3 border-blue-purple" />
+      </div>
+    </>
+  );
+};
+
+export default FriendListItem;
---
+++ src/components/SearchUserItem.jsx
@@ -0,0 +1,34 @@
+import GameLogo from "./wrapers/news/GameLogo";
+import GameName from "./wrapers/news/GameName";
+import { UserMinusIcon, UserPlusIcon } from "@heroicons/react/24/outline";
+
+function SearchUserItem({ user }) {
+  return (
+    <>
+      <div key={user.id}>
+        <div className="w-full">
+          <div className="flex justify-between items-center">
+            <div className="flex justify-between space-x-3">
+              <GameLogo src={user.userAvatar} />
+              <div>
+                <GameName text={user.nickName} />
+                {user.status === "offline" ? (
+                  <span className="text-red-500">offline</span>
+                ) : (
+                  <span className="text-green-500">online</span>
+                )}
+              </div>
+            </div>
+            <div className="flex space-x-4">
+              <UserMinusIcon className="h-8 xxl:h-12 text-red-500" />
+              <UserPlusIcon className="h-8 xxl:h-12 text-green-500" />
+            </div>
+          </div>
+        </div>
+        <div className="border my-3 border-blue-purple" />
+      </div>
+    </>
+  );
+}
+
+export default SearchUserItem
