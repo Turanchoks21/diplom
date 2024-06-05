b0701eb7070648c029a89972d67eab2e1074a525
Turanchoks21 <manjak2111@gmail.com>
2024-06-05 13:54:37 +0300

    feat(views): created settings page

---
+++ src/views/SettingsView.jsx
@@ -0,0 +1,11 @@
+function SettingsView() {
+  return (
+    <>
+      <div className="flex h-screen justify-center text-white font-semibold">
+        settings page
+      </div>
+    </>
+  );
+}
+
+export default SettingsView;
