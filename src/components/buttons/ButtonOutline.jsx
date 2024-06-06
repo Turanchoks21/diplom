4df363bcda8d5b888c00ff15a5a83077584e73f0
Turanchoks21 <manjak2111@gmail.com>
2024-06-07 01:14:18 +0300

    refactor

--- src/components/buttons/ButtonOutline.jsx
+++ src/components/buttons/ButtonOutline.jsx
@@ -1,19 +1,33 @@
 import { Link } from "react-router-dom";
 
 function ButtonOutline({ to, children, type, onClick }) {
+  if (to) {
+    return (
+      <Link
+        to={to}
+        className="p-2 xxl:p-3 w-full font-semibold text-center text-lg md:text-xl
+        xxl:text-3xl text-midnight-black dark:text-pale-yellow bg-clip border-2
+      border-midnight-black dark:border-pale-yellow hover:text-blue-purple
+      dark:hover:text-blue-purple hover:border-blue-purple dark:hover:border-blue-purple rounded-xl"
+      >
+        <button onClick={onClick} type={type}>
+          {children}
+        </button>
+      </Link>
+    );
+  }
+
   return (
-    <Link
-      to={to}
-      className="p-2 xxl:p-3 w-full font-semibold text-center text-lg md:text-xl xxl:text-3xl
-      text-midnight-black dark:text-pale-yellow bg-clip 
-      border-2 border-midnight-black
-      dark:border-pale-yellow hover:text-blue-purple
-      dark:hover:text-blue-purple hover:border-blue-purple 
-      dark:hover:border-blue-purple rounded-xl
-      "
+    <button
+      onClick={onClick}
+      type={type}
+      className="p-2 xxl:p-3 w-full font-semibold text-center text-lg md:text-xl
+      xxl:text-3xl text-midnight-black dark:text-pale-yellow bg-clip border-2
+    border-midnight-black dark:border-pale-yellow hover:text-blue-purple
+    dark:hover:text-blue-purple hover:border-blue-purple dark:hover:border-blue-purple rounded-xl"
     >
-      <button onClick={onClick} type={type}>{children}</button>
-    </Link>
+      {children}
+    </button>
   );
 }
 
--- src/components/buttons/ButtonSolid.jsx
+++ src/components/buttons/ButtonSolid.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
 function ButtonSolid({ to, children, type, onClick }) {
-  return (
-    <>
+  if (to) {
+    return (
       <Link
         to={to}
         className="py-2 xxl:py-3 w-full text-center text-lg md:text-xl xxl:text-3xl 
@@ -11,9 +11,25 @@ function ButtonSolid({ to, children, type, onClick }) {
           dark:border-pale-yellow hover:bg-blue-purple dark:hover:bg-blue-purple
           hover:border-blue-purple dark:hover:border-blue-purple rounded-xl"
       >
-        <button onClick={onClick} type={type}>{children}</button>
+        <button onClick={onClick} type={type}>
+          {children}
+        </button>
       </Link>
-    </>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      className="py-2 xxl:py-3 w-full text-center text-lg md:text-xl xxl:text-3xl 
+      text-lavender-mist dark:text-midnight-black
+        font-semibold bg-midnight-black dark:bg-pale-yellow border-2 border-midnight-black 
+      dark:border-pale-yellow hover:bg-blue-purple dark:hover:bg-blue-purple
+      hover:border-blue-purple dark:hover:border-blue-purple rounded-xl"
+    >
+      {children}
+    </button>
   );
 }
 
