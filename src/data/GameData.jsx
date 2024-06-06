9245ab1cf87641639b5bfcb83804027d08d2c69b
Turanchoks21 <manjak2111@gmail.com>
2024-06-07 01:15:23 +0300

    feat(data): created games data

---
+++ src/data/GameData.jsx
@@ -0,0 +1,96 @@
+function GameData() {
+  return [
+    {
+      id: 1,
+      name: "Brawlhalla",
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTlaQHb1dAQP5ij0lGbGZXGJoH-XlMCKG_-Q&s",
+      preference: true,
+    },
+    {
+      id: 2,
+      name: "MultiVersus",
+      logo: "https://pbs.twimg.com/profile_images/1767177446518902784/6xQX33hp_400x400.png",
+      preference: false,
+    },
+    {
+      id: 3,
+      name: "Skullgirls Mobile",
+      logo: "https://pbs.twimg.com/profile_images/1707527647532552192/-415OYhP_400x400.jpg",
+      preference: true,
+    },
+    {
+      id: 4,
+      name: "Fortnite",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/7/7c/Fortnite_F_lettermark_logo.png",
+      preference: true,
+    },
+    {
+      id: 5,
+      name: "Apex Legends",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Apex_legends_simple_logo.jpg",
+      preference: false,
+    },
+    {
+      id: 6,
+      name: "Overwatch 2",
+      logo: "https://i.pinimg.com/originals/a0/87/b1/a087b1bf735af93cd63f158ff7eb2b42.jpg",
+      preference: true,
+    },
+    {
+      id: 7,
+      name: "Genshin Impact",
+      logo: "https://i.pinimg.com/736x/47/45/2a/47452aa1e1c792e46be7a212042e69e5.jpg",
+      preference: true,
+    },
+    {
+      id: 8,
+      name: "Valorant",
+      logo: "https://seeklogo.com/images/V/valorant-logo-FAB2CA0E55-seeklogo.com.png",
+      preference: false,
+    },
+    {
+      id: 9,
+      name: "League of Legends",
+      logo: "https://i.pinimg.com/originals/d1/b1/1d/d1b11d5e4dbae547ac0d651476cec488.png",
+      preference: true,
+    },
+    {
+      id: 10,
+      name: "Minecraft",
+      logo: "https://thumbs.dreamstime.com/b/minecraft-logo-online-game-dirt-block-illustrations-concept-design-isolated-186775550.jpg",
+      preference: true,
+    },
+    {
+      id: 11,
+      name: "Among Us",
+      logo: "https://i.redd.it/jfv36ccxq8s51.png",
+      preference: false,
+    },
+    {
+      id: 12,
+      name: "FIFA 23",
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTj_8s_KfdEyR2wH_vFkL2iMskOizro4ViNCQ&s",
+      preference: false,
+    },
+    {
+      id: 13,
+      name: "Call of Duty: Warzone",
+      logo: "https://seeklogo.com/images/C/call-of-duty-warzone-mobile-logo-06EBE37B67-seeklogo.com.png?v=638351204560000000",
+      preference: true,
+    },
+    {
+      id: 14,
+      name: "Rocket League",
+      logo: "https://image.api.playstation.com/cdn/UP2002/CUSA01163_00/zFCya8WS8wjA50KudMeiqN8pRCNfdYtv.png?w=440&thumb=false",
+      preference: false,
+    },
+    {
+      id: 15,
+      name: "Cyberpunk 2077",
+      logo: "https://mir-s3-cdn-cf.behance.net/project_modules/1400/81a4e680815973.5cec6bcf6aa1a.jpg",
+      preference: true,
+    },
+  ];
+}
+
+export default GameData;
