11ce0c3da0bbafc68bb09e19be6add026fb51631
Turanchoks21 <manjak2111@gmail.com>
2024-06-08 17:54:49 +0300

    feat(componnets/inputs): added props for validation

--- src/components/inputs/FormInput.jsx
+++ src/components/inputs/FormInput.jsx
@@ -1,12 +1,22 @@
-function FormInput({ children, type }) {
+function FormInput({ children, type, name, id, register, error }) {
   return (
-    <div className="form-group w-full">
+    <div className="form-group w-full mb-4">
       <input
-        className="py-2 pl-3 w-full text-xl text-pale-yellow bg-midnight-black border-2 border-pale-yellow rounded-xl"
+        className={`py-2 pl-3 text-xl border-2  ${
+          error ? "border-red-500" : "border-blue-purple"
+        } rounded-xl`}
         type={type}
+        name={name}
+        id={id}
         placeholder={children}
+        {...register(name)}
       />
-      <label className="form-label">{children}</label>
+      <label
+        className={`form-label ${error ? "text-red-600" : "text-blue-purple"}`}
+      >
+        {children}
+      </label>
+      {error && <p className="mt-1 text-sm text-red-600">{error.message}</p>}
     </div>
   );
 }
