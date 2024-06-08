dbccefe84a3fb4b20ea429a56be6351b7e17506c
Turanchoks21 <manjak2111@gmail.com>
2024-06-08 17:55:13 +0300

    feat(views): added validation

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -6,24 +6,69 @@ import TitleWraper from "../components/forms/TitleWraper";
 import ButtonOutline from "../components/buttons/ButtonOutline";
 import ButtonSolid from "../components/buttons/ButtonSolid";
 import ButtonWraper from "../components/forms/ButtonWraper";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function LoginView() {
   const { t } = useTranslation();
 
-  return (
-    <>
+  const LoginSchema = Yup.object().shape({
+    nick_name: Yup.string()
+      .required(t("emptyField"))
+      .min(3, t("errorNick"))
+      .max(16, t("errorNick")),
+    password: Yup.string()
+      .required(t("emptyField"))
+      .min(8, t("errorPassword"))
+      .max(32, t("errorPassword")),
+  });
+
+  const LoginForm = () => {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({
+      resolver: yupResolver(LoginSchema),
+    });
+
+    const onSubmit = (data) => {
+      console.log(data);
+    };
+
+    return (
       <FormWraper>
         <TitleWraper>{t("loginTitle")}</TitleWraper>
-        <FormInput>{t("nickname")}</FormInput>
-        <FormInput type="password">{t("password")}</FormInput>
-        <ButtonWraper>
-          <ButtonSolid to="/main">{t("loginUser")}</ButtonSolid>
-          <ButtonOutline>{t("forgotPassword")}</ButtonOutline>
-          <ButtonOutline to="/register">{t("createUser")}</ButtonOutline>
-        </ButtonWraper>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <FormInput
+            name="nick_name"
+            id="nick_name"
+            register={register}
+            error={errors.nick_name}
+          >
+            {t("nickname")}
+          </FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            register={register}
+            error={errors.password}
+          >
+            {t("password")}
+          </FormInput>
+          <ButtonWraper>
+            <ButtonSolid type="submit">{t("loginUser")}</ButtonSolid>
+            <ButtonOutline>{t("forgotPassword")}</ButtonOutline>
+            <ButtonOutline to="/register">{t("createUser")}</ButtonOutline>
+          </ButtonWraper>
+        </form>
       </FormWraper>
-    </>
-  );
+    );
+  };
+
+  return <LoginForm />;
 }
 
 export default LoginView;
--- src/views/RegisterView.jsx
+++ src/views/RegisterView.jsx
@@ -6,24 +6,89 @@ import TitleWraper from "../components/forms/TitleWraper";
 import ButtonOutline from "../components/buttons/ButtonOutline";
 import ButtonSolid from "../components/buttons/ButtonSolid";
 import ButtonWraper from "../components/forms/ButtonWraper";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function RegisterView() {
   const { t } = useTranslation();
 
-  return (
-    <FormWraper>
-      <TitleWraper>{t("registerTitle")}</TitleWraper>
-      <FormInput>{t("nickname")}</FormInput>
-      <FormInput>Email</FormInput>
-      <FormInput type="password">{t("password")}</FormInput>
-      <FormInput type="password">{t("password")}</FormInput>
-      <FormInput>{t("data")}</FormInput>
-      <ButtonWraper>
-        <ButtonSolid to="/main">{t("registerUser")}</ButtonSolid>
-        <ButtonOutline to="/login">{t("haveAccount")}</ButtonOutline>
-      </ButtonWraper>
-    </FormWraper>
-  );
+  const RegisterSchema = Yup.object().shape({
+    nick_name: Yup.string()
+      .required(t("emptyField"))
+      .min(3, t("errorNick"))
+      .max(16, t("errorNick")),
+    email: Yup.string().email(t("invalidEmail")).required(t("emptyField")),
+    password: Yup.string()
+      .required(t("emptyField"))
+      .min(8, t("errorPassword"))
+      .max(32, t("errorPassword")),
+    passwordConfirm: Yup.string()
+      .required(t("emptyField"))
+      .oneOf([Yup.ref("password")], t("errorPasswordConfirm")),
+  });
+
+  const RegisterForm = () => {
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm({
+      resolver: yupResolver(RegisterSchema),
+    });
+
+    const onSubmit = (data) => {
+      console.log(data);
+    };
+
+    return (
+      <FormWraper>
+        <TitleWraper>{t("registerTitle")}</TitleWraper>
+        <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+          <FormInput
+            name="nick_name"
+            id="nick_name"
+            register={register}
+            error={errors.nick_name}
+          >
+            {t("nickname")}
+          </FormInput>
+          <FormInput
+            name="email"
+            id="email"
+            register={register}
+            error={errors.email}
+          >
+            Email
+          </FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            register={register}
+            error={errors.password}
+          >
+            {t("password")}
+          </FormInput>
+          <FormInput
+            type="password"
+            name="passwordConfirm"
+            id="passwordConfirm"
+            register={register}
+            error={errors.passwordConfirm}
+          >
+            {t("confirmPassword")}
+          </FormInput>
+          <ButtonWraper>
+            <ButtonSolid type="submit">{t("registerUser")}</ButtonSolid>
+            <ButtonOutline to="/login">{t("haveAccount")}</ButtonOutline>
+          </ButtonWraper>
+        </form>
+      </FormWraper>
+    );
+  };
+
+  return <RegisterForm />;
 }
 
 export default RegisterView;
