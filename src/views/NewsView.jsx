6fa5178fcf4e7b09c72158beef33b11fb8a1658a
Turanchoks21 <manjak2111@gmail.com>
2024-06-10 23:13:22 +0300

    feat(views): added check news

--- src/views/NewsView.jsx
+++ src/views/NewsView.jsx
@@ -12,10 +12,14 @@ import {
 import { HeartIcon as HeartIconSolid } from "@heroicons/react/20/solid";
 import { useState } from "react";
 import NewsData from "./../data/NewsData";
+import { useTranslation } from "react-i18next";
+import ButtonSolid from "./../components/buttons/ButtonSolid";
 
 function NewsView() {
   const [likes, setLikes] = useState({});
-  const posts = NewsData();
+  const { t } = useTranslation();
+  // const posts = NewsData();
+  const posts = [];
 
   function handleLikeClick(postId) {
     setLikes((prevLikes) => ({
@@ -26,43 +30,58 @@ function NewsView() {
 
   return (
     <>
-      <div className="flex flex-col w-full lg:max-w-5xl xxl:max-w-7xl mx-auto space-y-4">
-        {posts.map((post) => (
-          <NewsPost key={post.id}>
-            <div className="flex justify-between">
-              <div className="flex justify-between space-x-3">
-                <GameLogo src={post.gameLogo} />
-                <GameName text={post.gameName} />
+      <div className=" w-full lg:max-w-5xl xxl:max-w-7xl mx-auto ">
+        {posts.length !== 0 ? (
+          <div className="flex flex-col space-y-4">
+            {posts.map((post) => (
+              <NewsPost key={post.id}>
+                <div className="flex justify-between">
+                  <div className="flex justify-between space-x-3">
+                    <GameLogo src={post.gameLogo} />
+                    <GameName text={post.gameName} />
+                  </div>
+                  <div className="flex flex-col items-end">
+                    {post.times.map((time, index) => (
+                      <NewsTime key={index} time={time} />
+                    ))}
+                  </div>
+                </div>
+                <div className="text-midnight-black dark:text-white xxl:text-2xl">
+                  {post.content}
+                </div>
+                <div className="flex justify-center">
+                  <NewsPhoto src={post.photo} />
+                </div>
+                <div className="flex justify-between">
+                  <NewsButton>
+                    <ChatBubbleLeftEllipsisIcon className="h-8 xxl:h-12" />
+                  </NewsButton>
+                  <NewsButton>
+                    <ArrowsUpDownIcon className="h-8 xxl:h-12" />
+                  </NewsButton>
+                  <NewsButton onClick={() => handleLikeClick(post.id)}>
+                    {likes[post.id] ? (
+                      <HeartIconSolid className="h-8 xxl:h-12 text-red-500" />
+                    ) : (
+                      <HeartIcon className="h-8 xxl:h-12" />
+                    )}
+                  </NewsButton>
+                </div>
+              </NewsPost>
+            ))}
+          </div>
+        ) : (
+          <div className="text-midnight-black dark:text-pale-yellow font-semibold text-xl xxl:text-3xl">
+            <NewsPost>
+              <div className="flex justify-center items-center py-12">
+                <div className="flex flex-col items-center space-y-4">
+                  <span>{t("noNews")}</span>
+                  <ButtonSolid to="/main/settings">{t("addNews")}</ButtonSolid>
+                </div>
               </div>
-              <div className="flex flex-col items-end">
-                {post.times.map((time, index) => (
-                  <NewsTime key={index} time={time} />
-                ))}
-              </div>
-            </div>
-            <div className="text-midnight-black dark:text-white xxl:text-2xl">
-              {post.content}
-            </div>
-            <div className="flex justify-center">
-              <NewsPhoto src={post.photo} />
-            </div>
-            <div className="flex justify-between">
-              <NewsButton>
-                <ChatBubbleLeftEllipsisIcon className="h-8 xxl:h-12" />
-              </NewsButton>
-              <NewsButton>
-                <ArrowsUpDownIcon className="h-8 xxl:h-12" />
-              </NewsButton>
-              <NewsButton onClick={() => handleLikeClick(post.id)}>
-                {likes[post.id] ? (
-                  <HeartIconSolid className="h-8 xxl:h-12 text-red-500" />
-                ) : (
-                  <HeartIcon className="h-8 xxl:h-12" />
-                )}
-              </NewsButton>
-            </div>
-          </NewsPost>
-        ))}
+            </NewsPost>
+          </div>
+        )}
       </div>
     </>
   );
