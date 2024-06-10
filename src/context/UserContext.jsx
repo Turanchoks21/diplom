82763aea8d0f0bc3275bd9d7ced2fdb3f3dc2fc0
Turanchoks21 <manjak2111@gmail.com>
2024-06-10 22:07:53 +0300

    feat(context): added function crear user

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -7,12 +7,17 @@ export const useUsers = () => useContext(UsersContext);
 export function UsersProvider({ children }) {
   const [users, setUsers] = useState([]);
 
-  const addUser = (user) => {
+  function addUser(user) {
     setUsers((prevUsers) => [...prevUsers, user]);
-  };
+  }
+
+  function clearUsers() {
+    setUsers([]);
+    localStorage.removeItem("users");
+  }
 
   return (
-    <UsersContext.Provider value={{ users, addUser }}>
+    <UsersContext.Provider value={{ users, addUser, clearUsers }}>
       {children}
     </UsersContext.Provider>
   );
