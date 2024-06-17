56d6ac7960673e48e998affb0812cf42892744f7
Turanchoks21 <manjak2111@gmail.com>
2024-06-17 23:39:52 +0300

    feat(components/buttons): added check Link

--- src/components/buttons/NavButton.jsx
+++ src/components/buttons/NavButton.jsx
@@ -1,17 +1,32 @@
 import { Link } from "react-router-dom";
 
 function NavButton({ to, children, onClick }) {
+  if (to) {
+    return (
+      <>
+        <Link
+          to={to}
+          onClick={onClick}
+          className="flex items-center space-x-2
+        dark:text-pale-yellow font-semibold text-midnight-black hover:text-blue-purple  
+        dark:hover:text-blue-purple text-xl xxl:text-3xl 4xl:text-4xl transition ease-in-out hover:scale-110"
+        >
+          {children}
+        </Link>
+      </>
+    );
+  }
+
   return (
     <>
-      <Link
-        to={to}
+      <button
         onClick={onClick}
         className="flex items-center space-x-2
         dark:text-pale-yellow font-semibold text-midnight-black hover:text-blue-purple  
         dark:hover:text-blue-purple text-xl xxl:text-3xl 4xl:text-4xl transition ease-in-out hover:scale-110"
       >
         {children}
-      </Link>
+      </button>
     </>
   );
 }
