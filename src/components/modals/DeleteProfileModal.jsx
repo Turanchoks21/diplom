c8d85594d82dd86185e6bfe4c3822ce83a495e96
Turanchoks21 <manjak2111@gmail.com>
2024-06-18 19:13:33 +0300

    refactor

--- src/components/modals/DeleteProfileModal.jsx
+++ src/components/modals/DeleteProfileModal.jsx
@@ -18,31 +18,29 @@ function DeleteProfileModal({ isOpen, onClose }) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed z-30 inset-0 overflow-y-auto">
+    <div className="fixed h-screen z-30 inset-0 overflow-y-auto">
       <div
-        onClick={onClose}
-        className="flex items-center bg-gray-800 bg-opacity-75 transition-opacity
-       justify-center min-h-screen text-xl xxl:text-3xl 4xl:text-4xl"
+        className="flex items-center justify-center min-h-screen 
+      text-midnight-black dark:text-pale-yellow font-semibold"
       >
         <div
-          className="bg-lavender-mist dark:bg-midnight-black rounded-xl
-          overflow-hidden shadow-xl transform transition-all max-w-2xl w-full border-2 border-blue-purple"
+          className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+          onClick={onClose}
+        ></div>
+        <div
+          className="bg-lavender-mist dark:bg-midnight-black rounded-xl overflow-hidden shadow-xl
+          transform transition-all max-w-2xl w-full border-2 border-blue-purple"
         >
-          <div
-            className="bg-lavender-mist dark:bg-midnight-black px-4 py-5 border-b-2
-           border-blue-purple sm:px-6 sm:py-4"
-          >
+          <div className="bg-lavender-mist dark:bg-midnight-black px-4 py-5 border-b-2 border-blue-purple sm:px-6 sm:py-4">
             <div className="flex justify-between items-center">
-              <span
-                className=" leading-6 font-semibold text-midnight-black
-               dark:text-pale-yellow"
-              >
+              <span className="text-xl xxl:text-3xl leading-6 ">
                 {t("deleteUser")}
               </span>
               <button
                 type="button"
-                className="text-midnight-black dark:text-pale-yellow focus:outline-none 
-                focus:ring-0 focus:ring-offset-0"
+                className=" 
+                focus:outline-none focus:ring-0 focus:ring-offset-0"
                 onClick={onClose}
               >
                 <XMarkIcon className="h-8 xxl:h-12" />
@@ -53,16 +51,10 @@ function DeleteProfileModal({ isOpen, onClose }) {
             <span>{t("wantToDelete")}</span>
             <FaceFrownIcon className="h-8 xxl:h-12" />
           </div>
-          <div
-            className="bg-lavender-mist dark:bg-midnight-black border-t-2 border-blue-purple
-           px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"
-          >
+          <div className="bg-lavender-mist dark:bg-midnight-black border-t-2 border-blue-purple px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <div className="sm:pl-4">
               <button
-                className="p-2 xxl:p-3 w-full text-center text-lg md:text-xl xxl:text-3xl 
-              text-lavender-mist dark:text-midnight-black
-                font-bold bg-red-500 border-2 border-red-500 
-               hover:bg-red-600 hover:border-red-600 rounded-xl"
+                className="p-2 xxl:p-3 w-full text-center text-lg md:text-xl xxl:text-3xl text-lavender-mist dark:text-midnight-black font-bold bg-red-500 border-2 border-red-500 hover:bg-red-600 hover:border-red-600 rounded-xl"
                 type="button"
                 onClick={handleDeleteProfile}
               >
--- src/components/modals/ProfileSettingsModal.jsx
+++ src/components/modals/ProfileSettingsModal.jsx
@@ -61,31 +61,29 @@ function ProfileSettingsModal({ isOpen, onClose, userIndex }) {
   if (!isOpen || !user) return null;
 
   return (
-    <div className="fixed z-30 inset-0 overflow-y-auto">
+    <div className="fixed h-screen z-30 inset-0 overflow-y-auto">
       <div
-        className="flex items-center bg-gray-800 bg-opacity-75 transition-opacity
-       justify-center min-h-screen"
+        className="flex items-center justify-center min-h-screen 
+      text-midnight-black dark:text-pale-yellow font-semibold"
       >
-        <div className="fixed" aria-hidden="true" onClick={onClose}></div>
         <div
-          className="bg-lavender-mist dark:bg-midnight-black rounded-xl
-          overflow-hidden shadow-xl transform transition-all max-w-2xl w-full border-2 border-blue-purple"
+          className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity"
+          aria-hidden="true"
+          onClick={onClose}
+        ></div>
+        <div
+          className="bg-lavender-mist dark:bg-midnight-black rounded-xl overflow-hidden shadow-xl
+          transform transition-all max-w-2xl w-full border-2 border-blue-purple"
         >
-          <div
-            className="bg-lavender-mist dark:bg-midnight-black px-4 py-5 border-b-2
-           border-blue-purple sm:px-6 sm:py-4"
-          >
+          <div className="bg-lavender-mist dark:bg-midnight-black px-4 py-5 border-b-2 border-blue-purple sm:px-6 sm:py-4">
             <div className="flex justify-between items-center">
-              <span
-                className="text-xl xxl:text-3xl leading-6 font-semibold text-midnight-black
-               dark:text-pale-yellow"
-              >
-                {t("preferences")}
+              <span className="text-xl xxl:text-3xl leading-6 ">
+                {t("userSettings")}
               </span>
               <button
                 type="button"
-                className="text-midnight-black dark:text-pale-yellow focus:outline-none 
-                focus:ring-0 focus:ring-offset-0"
+                className=" 
+                focus:outline-none focus:ring-0 focus:ring-offset-0"
                 onClick={onClose}
               >
                 <XMarkIcon className="h-8 xxl:h-12" />
@@ -144,10 +142,7 @@ function ProfileSettingsModal({ isOpen, onClose, userIndex }) {
               </div>
             </div>
           </div>
-          <div
-            className="bg-lavender-mist dark:bg-midnight-black border-t-2 border-blue-purple
-           px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"
-          >
+          <div className="bg-lavender-mist dark:bg-midnight-black border-t-2 border-blue-purple px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
             <div className="sm:pl-4">
               {Object.values(isEditing).some((editing) => editing) ? (
                 <ButtonSolid type="button" onClick={handleSave}>
